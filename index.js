a22dcbef2d314b24071cc2e226f7970f1c4f5d76
Seydina Mandione Mbaye <121698929+Mandionelaye@users.noreply.github.com>
2023-11-15 01:27:50 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const express = require('express')
+const expres = require("express");
+const body = require('body-parser');
+const cors = require('cors');
+const routes = require('./route');
+const app = expres();
+
+app.use(body.urlencoded({extended:false}));
+app.use(body.json());
+app.use(cors());
+app.use(routes);
+
+app.listen(9000, ()=>{
+    console.log("http//localhost:9090");
+})const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
