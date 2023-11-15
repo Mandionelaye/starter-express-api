1c2e7cab122b0b2481e055bb88402cadc040e5fa
Seydina Mandione Mbaye <121698929+Mandionelaye@users.noreply.github.com>
2023-11-15 02:31:42 +0000

    Update email.js

--- email.js
+++ email.js
@@ -1,13 +1,7 @@
 const email = require("nodemailer")
 
 const sendMail= (req,res)=>{
-    const elm = {nom:"mbaye",
-prenom:"Seydina",
-numero:770938816,
-categori:"studio",
- evnement:"bapteme", date:"27/02/2030",
- heure:"10:00"}
-    const {nom,prenom,numero,categori, evnement, date, heure} = elm
+    const {nom,prenom,numero,categori, evnement, date, heure} =  req.body
     const transporter = email.createTransport({
     service:"gmail",
     auth:{
