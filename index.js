3efb55ddf95df187b47f0772202fd41ffa3f8aa3
Seydina Mandione Mbaye <121698929+Mandionelaye@users.noreply.github.com>
2024-04-25 18:21:43 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-const expres = require("express");
-const body = require('body-parser');
+const express = require('express');
+// const body = require('body-parser');
 const cors = require('cors');
-const routes = require('./route');
-const app = expres();
+const db  = require('./BasesDeDonnees/BD');
+const routes = require('./Routes/Route');
+require('dotenv').config();
 
-app.use(body.urlencoded({extended:false}));
-app.use(body.json());
+db.connection();
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
 app.use(cors());
 app.use(routes);
 
-app.listen(9000, ()=>{
-    console.log("http//localhost:9090");
-})
+app.listen(8000, ()=>(
+    console.log(`http://localhost:${port}`)
+))
